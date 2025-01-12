5e972a7ebf4664286b60a2c9bf12a62080da907c
mcaltabiano <rocket.rabbit@tutanota.com>
2025-01-12 18:24:30 +0100

    Provide custom prompt to assistant

--- src/assistant.py
+++ src/assistant.py
@@ -1,9 +1,12 @@
 import gradio as gr
 from langchain_ollama.llms import OllamaLLM
+from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores.base import VectorStore
+
 from typing import Optional
+import yaml
 
 class Assistant:
     def __init__(
@@ -26,15 +29,28 @@ class Assistant:
         self.conversation_chain = None
         self.interface = None
         
+        self._read_config()
         self._setup_chain()
     
+    def _read_config(self):
+        with open('config.yaml', 'r') as file:
+            config = yaml.safe_load(file)
+
+        self.custom_prompt = config.get('assistant', {}).get('custom_prompt', None)
+
     def _setup_chain(self):
         """Configura la catena di conversazione con LLM, retriever e memoria."""
         llm = OllamaLLM(model=self.model_name)
-        memory = ConversationBufferMemory(
-            memory_key='chat_history',
-            return_messages=True
-        )
+        
+        custom_prompt = getattr(self, 'custom_prompt', None)
+        params = {
+            "memory_key": "chat_history",
+            "return_messages": True,
+            **({"prompt": custom_prompt} if custom_prompt else {})
+        }
+
+        memory = ConversationBufferMemory(**params)
+
         retriever = self.vectorstore.as_retriever(
             search_kwargs={"k": self.retriever_k}
         )
@@ -78,7 +94,8 @@ class Assistant:
         self.interface = gr.ChatInterface(
             fn=self._chat,
             title="Puffo assistente",
-            description="Fai domande sulla tua knowledge base"
+            description="Fai domande sulla tua knowledge base",
+            type="messages"
         )
         
         self.interface.launch(
